563265fce5bc522e664c45bc762accc6aeffb353
Samuel Oshio <samueloshio5@gmail.com>
2024-04-20 13:00:25 +0000

    DAGs dependencies updated

--- dags/estore.py
+++ dags/estore.py
@@ -5,6 +5,7 @@ from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesyste
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
 from astro import sql as aql
 from astro.files import File
+from airflow.models.baseoperator import chain
 from astro.sql.table import Table, Metadata
 from astro.constants import FileType
 from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
@@ -55,7 +56,7 @@ def estore():
 
         return check(scan_name, checks_subpath)
     
-    check_load()
+
 
     transform = DbtTaskGroup(
         group_id='transform',
@@ -66,6 +67,14 @@ def estore():
             select=['path:models/transform']
         )
     )
+
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_transform(scan_name='check_transform', checks_subpath='transform'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+    
 
     report = DbtTaskGroup(
         group_id='report',
@@ -78,19 +87,20 @@ def estore():
     )
 
     @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
-    def check_transform(scan_name='check_transform', checks_subpath='transform'):
-        from include.soda.check_function import check
-
-        return check(scan_name, checks_subpath)
-    
-    check_transform()
-
-    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
     def check_report(scan_name='check_report', checks_subpath='report'):
         from include.soda.check_function import check
 
         return check(scan_name, checks_subpath)
 
-    check_report()
+    chain(
+        upload_csv_to_gcs
+        create_retail_dataset
+        gcs_to_raw
+        check_load()
+        transform
+        check_transform()
+        report
+        check_report()
+    )
 
 estore()
