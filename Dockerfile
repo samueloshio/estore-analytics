c141d83ab83ae4ecf2b9cea7a20635e079e231ac
Samuel Oshio <samueloshio5@gmail.com>
2024-04-20 10:06:55 +0000

    Data Quality Check Updated

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,6 @@
 FROM quay.io/astronomer/astro-runtime:11.1.0
+
+# install soda into a virtual environment
+RUN python -m venv soda_venv && source soda_venv/bin/activate && \
+    pip install --no-cache-dir soda-core-bigquery==3.3.1 &&\
+    pip install --no-cache-dir soda-core-scientific==3.3.1 && deactivate
--- dags/estore.py
+++ dags/estore.py
@@ -46,4 +46,12 @@ def estore():
         use_native_support=False,
     )
 
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_load(scan_name='check_load', checks_subpath='sources'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+    
+    check_load()
+  
 estore()
